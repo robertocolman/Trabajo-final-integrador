f86ebf0c6f30389d7cc6b087ccd4cfff4bc4e928
nah.valenz@gmail.com <nah.valenz@gmail.com>
2026-05-07 14:24:49 -0300

    validaciones

--- src/routes/especialidadRoutes.js
+++ src/routes/especialidadRoutes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const especialidadController = require('../controllers/especialidadController');
-
-// 1. Importamos TUS validaciones subiendo un nivel (../) y entrando a tu carpeta
 const { validarEspecialidad, validarIdEspecialidad } = require('../validators/especialidadValidator');
 
-// 2. Inyectamos las validaciones en el medio de las peticiones
 router.get('/', especialidadController.getAllEspecialidades);
 
 router.get('/:id', validarIdEspecialidad, especialidadController.getEspecialidadById);
